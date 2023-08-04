d58e63ca9270a76b43eb150b832f12b6f175b998
BuildTools <unconfigured@null.spigotmc.org>
2023-08-04 22:35:13 +0100

    Added Animations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-
 import {getWeather} from "./weather";
 import {fishing_forecast} from "./FishFormula";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFishFins, faMapLocation, faTemperature3} from '@fortawesome/free-solid-svg-icons';
+import { motion } from "framer-motion";
 
 function App() {
     const [location, setLocation] = useState(null);
@@ -38,21 +40,21 @@ function App() {
     return (
         <div className="container">
             <header className="header">
-                <h1 className="headerText">Fishing Forecast</h1>
+                <h1 className="headerText"><FontAwesomeIcon icon={faFishFins} /> Fishing Forecast</h1>
             </header>
 
             <div className="locationContainer">
 
                 {location ? (
-                    <p className="locationText">Location: {location}</p>
+                    <p className="locationText"><FontAwesomeIcon icon={faMapLocation} /> Location: {location}</p>
                 ) : (
                     <p className="locationText">No location available</p>
                 )}
             </div>
 
-            <div className="weatherContainer">
+            <div className="tempContainer">
 
-                <p className="tempText">Temperature: {temperature}</p>
+                <p className="tempText"><FontAwesomeIcon icon={faTemperature3}/> Temperature: {temperature}</p>
             </div>
 
             <div className="weatherContainer">
@@ -60,21 +62,48 @@ function App() {
                 <p className="weatherText">Weather: {description}</p>
             </div>
 
-            <div className="weatherContainer">
-
-                <p className="weatherText">Current chance of bite: {forecast}%</p>
-            </div>
-
-            <div className="fish-chance-box">
-                <div className="header">Fishing Forecast</div>
-                <div className="percentage-display">
+            {forecast !== null ? (
+            <motion.div className="fish-chance-box"
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            type: 'spring',
+                            stiffness: 100,
+                            damping: 10,
+                            duration: 2
+                        }}
+            >
+                <motion.div className="header"
+                            initial={{ y: -20 }}
+                            animate={{ y: 0 }}
+                            transition={{
+                                delay: 1,
+                                type: 'spring',
+                                stiffness: 100
+                            }}
+                >
+                    Chance of a Bite
+                </motion.div>
+                <motion.div className="percentage-display"
+                            initial={{ y: 20 }}
+                            animate={{ y: 0 }}
+                            transition={{
+                                delay: 1,
+                                type: 'spring',
+                                stiffness: 100
+                            }}
+                >
                     {forecast}%
-                </div>
-                <div className="footer">Chance of Catching Fish</div>
-            </div>
+                </motion.div>
+            </motion.div>
+            ) : (
+                <div className="loading-bar-container">
+                <div className="loading-bar"></div>
+                </div>)}
+
 
             <footer className="footer">
-                <p className="footerText">Made by Jack Sinclair</p>
+                <p className="footerText">Developed by Jack Sinclair</p>
             </footer>
         </div>
     );
